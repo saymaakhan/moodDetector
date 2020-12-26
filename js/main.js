ff736707f12d05e25138cd83c5c497135a021339
saymaakhan <s489khan@uwaterloo.ca>
2020-12-25 21:05:51 -0500

    Add visuals for webcam from API to canvas

--- js/main.js
+++ js/main.js
@@ -22,6 +22,19 @@ video.addEventListener("playing", () => {
   const displaySize = { width: video.width, height: video.height };
   faceapi.matchDimensions(canvas, displaySize); //size canvas to video 
 
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100); //every 100 ms
 });
 
 
